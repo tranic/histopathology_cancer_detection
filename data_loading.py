9132b0de5f77d69400eff2f0bc6ed84dddb5cb21
Eric Fischer <eric.fischer95@hotmail.de>
2020-07-16 19:36:19 +0200

    added 3 channel image loading instead of greyscale

--- data_loading.py
+++ data_loading.py
@@ -3,19 +3,21 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import os
 import skimage.io as io
+import matplotlib.pyplot as plt
 
 class HistopathDataset(Dataset):
     """ Histopathologic Cancer Dataset that represents a map from keys to data samples."""
 
-    def __init__(self, label_file, root_dir, transform=None):
+    def __init__(self, label_file, root_dir, transform=None, greyscale=False):
         """
-        :param label_file: path to csv file containing labels for each and every image id.
+        :param label_file: path to csv file containing labels for each and every image id of the data set.
         :param root_dir:  path to directory with all images.
         :param transform: optional transformation operation on each sample.
         """
         self.data_frame = pd.read_csv(label_file)
         self.root_dir = root_dir
         self.transform = transform
+        self.greyscale = greyscale
 
     def __len__(self):
         return len(self.data_frame)
@@ -29,7 +31,7 @@ class HistopathDataset(Dataset):
 
         img_path = os.path.join(self.root_dir, self.data_frame.iloc[index, 0])
         img_path = img_path + ".tif"
-        image = io.imread(fname=img_path, as_gray=True) # np ndarray
+        image = io.imread(fname=img_path, as_gray=self.greyscale) # np ndarray
         label = self.data_frame.iloc[index, 1]
         sample = (image, label)
 
@@ -46,11 +48,11 @@ class ToTensor(object):
 
         # numpy image: H x W x C
         # torch image: C X H X W
-        # image = image.transpose((2, 0, 1)) for colored images
+        image = image.transpose((2, 0, 1)) # for colored images
         return (torch.from_numpy(image), label)
 
 if __name__ == '__main__':
-    # Example on how to use the HistopathDataset class
+    ## Example on how to use the HistopathDataset class
     num_workers = 0
     batchsize = 128
 
@@ -60,12 +62,13 @@ if __name__ == '__main__':
         root_dir=os.path.abspath("data/train"),
         transform=ToTensor())
 
-    # get images manually
+    # get images manually: plot first two images
     for i in range(len(transformed_dataset)):
         sample = transformed_dataset[i]
         print("index: ", i, " image size: ", sample[0].size(), " label: ", sample[1])
-
-        if i == 3: break
+        # imgplot = plt.imshow(sample[0])
+        # plt.show()
+        if i == 1: break
 
     # use DataLoader of torch
     dataloader = DataLoader(transformed_dataset, batch_size=batchsize, shuffle=True, num_workers=num_workers)
