59b9ee33e0ec304a1d9bfb6c7d47b0aec2d94ac8
christianwarmuth <chris.warmuth@t-online.de>
2020-07-24 20:49:58 +0200

    Updated Trainings-Loop

--- trainingloop.sh
+++ trainingloop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    for name in "densnet121" "ResNet" "asdasdasd"; do 
+    for name in 'vgg11' 'vgg19' 'densenet121' 'densenet201' 'resnet18' 'resnet152' 'resnet18_96' 'resnet152_96'; do 
         python3 server_model_training.py -trnl ./data/train_split.csv -tstl ./data/test_split.csv -f ./data/train -o ./data -m $name;
     done;
 done;
