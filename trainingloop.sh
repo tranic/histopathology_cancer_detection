99674bd8ae2a2d4d93827dcb99da06e08b4eb32b
christianwarmuth <chris.warmuth@t-online.de>
2020-07-25 15:17:54 +0200

    Changed Trainings-Loop
    Co-authored-by: Simon Witzke <witzke.simon@gmail.com>

--- trainingloop.sh
+++ trainingloop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    for name in 'vgg11' 'vgg19' 'densenet121' 'densenet201' 'resnet18' 'resnet152' 'resnet18_96' 'resnet152_96'; do 
-        python3 server_model_training.py -trnl ./data/train_split.csv -tstl ./data/test_split.csv -f ./data/train -o ./data -m $name;
+    for name in  'resnet152_96' 'resnet18_96' 'densenet121' 'densenet201' 'vgg11' 'vgg19'   ; do 
+        python3 -W ignore server_model_training.py -trnl ./data/train_split.csv -tstl ./data/test_split.csv -f ./data/train -o ./data -m $name;
     done;
 done;
