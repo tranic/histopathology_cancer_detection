8439d6a204a047b933f6457ec8de5d492563d21d
christianwarmuth <chris.warmuth@t-online.de>
2020-07-25 16:43:08 +0200

    Supressed Warnings

--- trainingloop_pretrained.sh
+++ trainingloop_pretrained.sh
@@ -2,6 +2,6 @@
 
 while true; do
     for name in 'densenet121' 'densenet201' 'resnet34' 'resnet152'; do 
-        python3 server_model_training_pretrained.py -trnl ./data/train_split.csv -tstl ./data/test_split.csv -f ./data/train -o ./data -m $name;
+        python3 -W ignore server_model_training_pretrained.py -trnl ./data/train_split.csv -tstl ./data/test_split.csv -f ./data/train -o ./data -m $name;
     done;
 done;
